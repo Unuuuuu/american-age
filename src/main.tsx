e1c7bf29a85771b67af2fdec874c00a1a874a768
Yunwoo Ji <unu12073@gmail.com>
2023-01-11 00:43:04 +0900

    Change font-family of mui to pretendard

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import { CssBaseline } from "@mui/material";
 import { Global } from "@emotion/react";
 import globalCss from "./styles/global";
 import "pretendard/dist/web/static/pretendard.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <CssBaseline />
     <Global styles={globalCss} />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
