a0c11c6ca130ec0f4bddcd1c4df6fb8be3928da3
Yunwoo Ji <unu12073@gmail.com>
2023-01-11 23:37:53 +0900

    Improve error handling

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import TextField from "@mui/material/TextField";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
-import Snackbar, { SnackbarProps } from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 import { css } from "@emotion/react";
 import Collapse from "@mui/material/Collapse";
@@ -25,7 +24,6 @@ type BirthdayType = Dayjs | null;
 const App = () => {
   const [birthday, setBirthday] = useState<BirthdayType>(null);
   const [westernAge, setWesternAge] = useState<number | null>(null);
-  const [isErrorSnackbarOpen, setIsErrorSnackbarOpen] = useState(false);
 
   const handleDatePickerChange = (newBirthday: BirthdayType) => {
     setBirthday(newBirthday);
@@ -34,13 +32,12 @@ const App = () => {
   const handleFormSubmit: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
 
-    // 값이 유효하지 않다면 error snackbar를 띄운다.
+    // 값이 유효하지 않은 경우에 대해 예외 처리를 한다.
     if (
       birthday === null ||
       !birthday.isValid() ||
       dayjs().isBefore(birthday)
     ) {
-      setIsErrorSnackbarOpen(true);
       return;
     }
 
@@ -49,15 +46,6 @@ const App = () => {
 
     // 계산된 만 나이로 업데이트한다.
     setWesternAge(westernAgeFromBirthday);
-    setIsErrorSnackbarOpen(false);
-  };
-
-  const handleErrorSnackbarClose: SnackbarProps["onClose"] = (_, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setIsErrorSnackbarOpen(false);
   };
 
   return (
@@ -79,9 +67,16 @@ const App = () => {
               value={birthday}
               onChange={handleDatePickerChange}
               renderInput={(params) => (
-                <TextField {...params} variant="filled" />
+                <TextField
+                  {...params}
+                  variant="filled"
+                  helperText={params.error ? "잘못된 생년월일입니다." : ""}
+                />
               )}
               disableFuture
+              openTo="year"
+              views={["year", "month", "day"]}
+              maxDate={dayjs()}
             />
             <Button variant="contained" disableElevation type="submit">
               계산하기
@@ -89,14 +84,6 @@ const App = () => {
           </Stack>
         </form>
       </Stack>
-      <Snackbar
-        open={isErrorSnackbarOpen}
-        autoHideDuration={5000}
-        onClose={handleErrorSnackbarClose}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-      >
-        <Alert severity="error">유효하지 않은 생년월일입니다.</Alert>
-      </Snackbar>
     </Container>
   );
 };
